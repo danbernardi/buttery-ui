1a8ffdc0d15deb6584a355dba7ff4a0aecc424b2
Daniel Bernardi <danbernardi1@gmail.com>
2024-08-11 11:28:56 -0700

    Adds first story - button component

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
 
-      <Button />
+      <Button>Click me</Button>
     </>
   )
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,9 @@
-const Button = () => {
+import type { ReactNode } from 'react';
+
+export const Button = ({children}: {children: ReactNode}) => {
   return (
     <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
-      Click me
+      {children}
     </button>
   )
 }
